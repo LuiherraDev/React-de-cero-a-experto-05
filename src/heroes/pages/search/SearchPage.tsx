10188a0650307c1cbc7be3c09b6ed1d97c4f62db
luis <luiherra25@gmail.com>
2026-07-06 16:48:48 +0200

    Thinking in components 01

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,12 @@
+import { CustomJumbotron } from "@/components/custom/customJumbotron"
+
 export const SearchPage = () => {
   return (
-    <div>SearchPage</div>
+    <>
+      <CustomJumbotron
+        title="Búsqueda de SuperHéroes"
+        description="Descubre, explora y administra super héroes y villanos" />
+    </>
   )
 }
 
