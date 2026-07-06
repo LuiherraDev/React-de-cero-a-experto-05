75f10ac0ad29f664f5a1be293a1252c3af1167c2
luis <luiherra25@gmail.com>
2026-07-06 17:57:33 +0200

    Thinking in components 03

--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,13 +1,18 @@
 import { CustomJumbotron } from "@/components/custom/CustomJumbotron"
 import { HeroStats } from "@/heroes/components/HeroStats"
+import { SearchControl } from "./ui/SearchControl"
 
 export const SearchPage = () => {
   return (
     <>
       <CustomJumbotron
         title="Búsqueda de SuperHéroes"
-        description="Descubre, explora y administra super héroes y villanos" />
+        description="Descubre, explora y administra super héroes y villanos"
+      />
+
       <HeroStats />
+
+      <SearchControl />
     </>
 
   )
