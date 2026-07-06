2bcd876d3ce5b420fd5f94841a40343c68ac0768
luis <luiherra25@gmail.com>
2026-07-06 16:11:05 +0200

    Using lazyl load in some routes

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,12 +1,27 @@
-import { Outlet } from "react-router"
+import { Link, Outlet } from "react-router"
 
 export const HeroesLayout = () => {
   return (
-    <div className="bg-red-500">
+    <div className="bg-red-400">
 
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/heroes/1'>Hero</Link>
+        </li>
+        <li>
+          <Link to='/search'>Searches</Link>
+        </li>
+        <li>
+          <Link to='/admin'>Admin</Link>
+        </li>
+      </ul>
 
-
-      <Outlet />
+      <section className="mt-10">
+        <Outlet />
+      </section>
     </div>
   )
 }
--- src/heroes/pages/router/app.router.tsx
+++ src/heroes/pages/router/app.router.tsx
@@ -1,11 +1,15 @@
+import { lazy } from "react";
 import { createBrowserRouter } from "react-router";
 
 import { HomePage } from '../home/HomePage';
 import { HeroPage } from "../hero/HeroPage";
 import { AdminPage } from "@/admin/pages/AdminPage";
-import { SearchPage } from "../search/SearchPage";
 import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
 import { AdminLayout } from "@/admin/layouts/AdminLayout";
+
+// import { SearchPage } from "../search/SearchPage";
+// const SearchPage = lazy(() => import("../search/SearchPage").then(module => ({ default: module.SearchPage }))  //añadimos export default en SearchPage y no hace falta poner .then....
+const SearchPage = lazy(() => import("../search/SearchPage"))
 
 
 export const appRouter = createBrowserRouter([
