2b7d21def71916365d880c4095c006ad07275314
luis <luiherra25@gmail.com>
2026-07-06 16:00:45 +0200

    Using layouts

--- src/heroes/pages/router/app.router.tsx
+++ src/heroes/pages/router/app.router.tsx
@@ -1,26 +1,42 @@
 import { createBrowserRouter } from "react-router";
+
 import { HomePage } from '../home/HomePage';
 import { HeroPage } from "../hero/HeroPage";
 import { AdminPage } from "@/admin/pages/AdminPage";
 import { SearchPage } from "../search/SearchPage";
+import { HeroesLayout } from "@/heroes/layouts/HeroesLayout";
+import { AdminLayout } from "@/admin/layouts/AdminLayout";
 
 
 export const appRouter = createBrowserRouter([
   {
     path: '/',
-    element: <HomePage />
+    element: <HeroesLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />
+      },
+      {
+        path: 'heroes/1',
+        element: <HeroPage />
+      },
+      {
+        path: 'search',
+        element: <SearchPage />
+      },
+    ]
   },
-  {
-    path: '/heroes/1',
-    element: <HeroPage />
-  },
-  {
-    path: '/search',
-    element: <SearchPage />
-  },
+
   {
     path: '/admin',
-    element: <AdminPage />
+    element: <AdminLayout />,
+    children: [
+      {
+        index: true,
+        element: <AdminPage />
+      }
+    ]
   },
 ])
 
